56d673ac57b48467d0ed2799af5ef98489346e66
Yash Lala <yashlala@gmail.com>
2022-07-01 16:57:17 -0700

    Print to dmesg after syscall.

--- extended_syscalls/extended_syscalls.c
+++ extended_syscalls/extended_syscalls.c
@@ -1,20 +1,21 @@
+#include <linux/swap.h>
 #include <linux/extended_syscalls.h> 
 #include <linux/cpuset.h> 
 
 SYSCALL_DEFINE1(isolate_swap, int, enable)
 {
-    // __isolate_swap(); 
+    pr_warn("isolate_swap syscall called.\n"); 
     return 0; 
 }
 
 SYSCALL_DEFINE1(set_cgroup_swap, int __user *, swap_info_struct_num)
 {
-    // __set_cgroup_swap(); 
+    pr_warn("set_cgroup_swap syscall called.\n"); 
     return 0; 
 }
 
 SYSCALL_DEFINE1(get_cgroup_swap, int __user *, swap_info_struct_num)
 {
-    // __get_cgroup_swap(); 
+    pr_warn("get_cgroup_swap syscall called.\n"); 
     return 0; 
 }
