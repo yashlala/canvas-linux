4fa22891aae65aa4cc03c4dbefdae6f21f5adca3
Yash Lala <yashlala@gmail.com>
2022-07-01 19:52:38 -0700

    Add cpuset accessors for when CONFIG_CPUSETS is not set.

--- include/linux/cpuset.h
+++ include/linux/cpuset.h
@@ -79,7 +79,7 @@ extern nodemask_t cpuset_mems_allowed(struct task_struct *p);
 #define cpuset_current_mems_allowed (current->mems_allowed)
 void cpuset_init_current_mems_allowed(void);
 int cpuset_nodemask_valid_mems_allowed(nodemask_t *nodemask);
-// TODO: Set on the other side of the ifdef!
+// TODO: REMEMBER the other side of the ifdef!
 // (ie: ifndef CONFIG_CPUSETS)
 extern struct swap_info_struct *cpuset_get_preferred_swap(struct task_struct *p); 
 extern void cpuset_set_preferred_swap(struct task_struct *p, 
@@ -305,6 +305,19 @@ static inline bool read_mems_allowed_retry(unsigned int seq)
 {
 	return false;
 }
+
+static inline swap_info_struct *cpuset_get_preferred_swap(struct task_struct *p)
+{
+	return NULL; 
+}
+
+static inline void cpuset_set_preferred_swap(struct task_struct *p, 
+		struct swap_info_struct *si) 
+{
+	return; 
+}
+#define cpuset_get_current_preferred_swap() cpuset_get_preferred_swap(current)
+#define cpuset_set_current_preferred_swap(si) cpuset_preferred_swap(current, si)
 
 #endif /* !CONFIG_CPUSETS */
 
